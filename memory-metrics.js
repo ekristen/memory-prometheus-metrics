4ca2f32e9623f753e0bf68af7c553a942f0def68
Erik Kristensen <erik@erikkristensen.com>
2015-10-30 14:45:59 -0600

    updating to prometheus 2.3.0

--- memory-metrics.js
+++ memory-metrics.js
@@ -18,47 +18,47 @@ function MemoryMetrics(client, opts) {
   this.memwatch = memwatch
 
   var metrics = {
-    num_full_gc: this.client.newGauge({
+    num_full_gc: this.client.createGauge({
       name: 'num_full_gcs_total',
       subsystem: 'nodejs_memory',
       help: 'Number of Full Garabage Collections'
     }),
-    num_inc_gc: this.client.newGauge({
+    num_inc_gc: this.client.createGauge({
       name: 'num_inc_gcs_total',
       subsystem: 'nodejs_memory',
       help: 'Number of Incremental Garabage Collections'
     }),
-    heap_compactions: this.client.newGauge({
+    heap_compactions: this.client.createGauge({
       name: 'heap_compactions_total',
       subsystem: 'nodejs_memory',
       help: 'Number of Heap Compactions'
     }),
-    estimated_base: this.client.newGauge({
+    estimated_base: this.client.createGauge({
       name: 'estimated_base',
       subsystem: 'nodejs_memory',
       help: 'Estimated base Memory Usage'
     }),
-    current_base: this.client.newGauge({
+    current_base: this.client.createGauge({
       name: 'current_base',
       subsystem: 'nodejs_memory',
       help: 'Current base Memory Usage'
     }),
-    min: this.client.newGauge({
+    min: this.client.createGauge({
       name: 'min',
       subsystem: 'nodejs_memory',
       help: 'Minimum Memory Usage'
     }),
-    max: this.client.newGauge({
+    max: this.client.createGauge({
       name: 'max',
       subsystem: 'nodejs_memory',
       help: 'Maximum Memory Usage'
     }),
-    usage_trend: this.client.newGauge({
+    usage_trend: this.client.createGauge({
       name: 'usage_trend',
       subsystem: 'nodejs_memory',
       help: 'Usage trend'
     }),
-    leaks: this.client.newGauge({
+    leaks: this.client.createGauge({
       name: 'memory_leaks_total',
       help: 'Total number of memory leaks'
     })
